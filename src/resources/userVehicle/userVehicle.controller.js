4d4718be0daa46efe872458e23e0c8a7a3a91ecd
Santiago <sagudecod97@gmail.com>
2020-03-06 13:22:00 -0500

    Created all the controllers of User vehicle model

---
+++ src/resources/userVehicle/userVehicle.controller.js
@@ -0,0 +1,90 @@
+const UserVehicle = require('./userVehicle.model')
+
+const createUserVehicle = async (req, res) => {
+    try {
+        const userVehicle = await UserVehicle
+        .create({...req.body})
+
+        res.status(201).json({data: userVehicle})
+    } catch(err) {
+        console.error(err)
+        res.status(400).json({'Error': 'The data to create vehicle is wrong'})
+    } 
+}
+
+const getUserVehicle = async (req, res) => {
+    try {
+        const userVehicle = await UserVehicle
+        .findOne({_id: req.params.id})
+        .lean()
+        .exec()
+
+        if (!userVehicle) {
+            res.status(400).json({'Error': 'The vehicle doesn\'t exist'})
+        }
+
+        res.status(200).json({data: userVehicle})
+    } catch(err) {
+        console.error(err)
+        res.status(400).json({'Error': 'Getting the vehicle of the user'})
+    }
+}
+
+const getUserVehicles = async (req, res) => {
+    try {
+        const userVehicles = await UserVehicle
+        .find({ owner: req.params.id})
+
+        res.status(200).json({data: userVehicles})
+    } catch (err) {
+        console.error(err)
+        res.status(400).json({'Error': ''})
+    } 
+}
+
+const updateUserVehicle = async (req, res) => {
+    try {
+        const updatedVehicle = await UserVehicle
+        .findByIdAndUpdate(
+            { _id: req.params.vehicle_id},
+            {...req.body},
+            {new: true}
+        )
+        .lean()
+        .exec()
+         
+        if (!updateUserVehicle) {
+            res.status(400).json({'Error': 'Couldn\'t update the vehicle'})
+        }
+
+        res.status(200).json({data: updatedVehicle})
+    } catch (err) {
+        console.error(err)
+        res.status(400).json({'Error': 'The vehicle id doesn\'t not exist'})
+    }
+}
+
+const deleteUserVehicle = async (req, res) => {
+    try {
+        const deletedVehicle = await UserVehicle
+        .findByIdAndRemove({_id: req.params.vehicle_id})
+        .exec()
+
+        if (!deletedVehicle) {
+            res.status(400).json({'Error': 'The vehicle id doesn\'t exist'})
+        }
+
+        res.status(200).json({dataDeleted: deletedVehicle})
+    } catch(err) {
+        console.error(err)
+        res.status(400).json({'Error': 'While deleting the vehicle'})
+    }
+}
+
+module.exports = { 
+    createUserVehicle, 
+    getUserVehicle,
+    getUserVehicles,
+    updateUserVehicle,
+    deleteUserVehicle
+}
