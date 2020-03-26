f9ea2e96265ce8d9e5c577dec3af7d8ae164acf6
Santiago Agudelo Álvarez <50092431+sagudecod97@users.noreply.github.com>
2020-03-25 22:20:49 -0500

    Comment the DB connection

--- utils/db.js
+++ utils/db.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
 const options = require('../config/serverConfig')
 
+// Defines the connection to the DB, if production or not
 const urlDB = typeof options.optionsDev !== 'undefined' ? options.optionsDev.dbURL : options.optionsProd.dbURL
 
+// Makes the connection with the DB
 const connectDB = (url = urlDB) => {
     return mongoose.connect(url, {
         useNewUrlParser: true,
