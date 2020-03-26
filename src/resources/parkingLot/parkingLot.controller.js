ee8c1ce5ae3049057ecfe1593bb02410207fc48d
Santiago Agudelo Álvarez <50092431+sagudecod97@users.noreply.github.com>
2020-03-26 00:44:37 -0500

    Commented the PARKING LOT controllers

--- src/resources/parkingLot/parkingLot.controller.js
+++ src/resources/parkingLot/parkingLot.controller.js
@@ -1,53 +1,57 @@
 const ParkingLot = require('./parkingLot.model')
 
+// Creates a parking lot
 const createParkingLot = async (req, res) => {
     try {
-        const parkingLot = await ParkingLot
+        const parkingLot = await ParkingLot // Creates the parking lot
         .create({...req.body})
 
-        return res.status(200).json({data: parkingLot})
+        return res.status(200).json({data: parkingLot}) // Return the created parkinglot data
 
     } catch (err) {
         console.error(err)
-        return res.status(400).json({'Error': 'the data to create parking lot is wrong'})
+        return res.status(400).json({'Error': 'the data to create parking lot is wrong'}) // Couldn't create the parking lot
     }
 }
 
+// Get a parking lot
 const getParkingLot = async (req, res) => {
     try {
-        const parkingLot = await ParkingLot
+        const parkingLot = await ParkingLot // Gets the required parking lot
         .findById({ _id: req.params.id })
         .lean()
         .exec()
 
-        if (!parkingLot) {
+        if (!parkingLot) { // Checks if the parking lot exists
             return res.status(400).json({'Error': 'Parking lot doesn\'t exist'})
         }
         
-        return res.status(200).json({ data: parkingLot})
+        return res.status(200).json({ data: parkingLot}) // Returns the required parking lot
     } catch (err) {
         console.error(err)
-        return res.status(400).json({'Error': 'Couldn\'t get parking lot'})
+        return res.status(400).json({'Error': 'Couldn\'t get parking lot'}) // Couldn't get the parking lot
     }
 }
 
+// Get All parking lots
 const getAllParkingLots = async (req, res) => {
     try {
-        const parkingLots = await ParkingLot
+        const parkingLots = await ParkingLot // Get all the parking lots
         .find({})
         .lean()
         .exec()
 
-        return res.status(200).json({ data: parkingLots})
+        return res.status(200).json({ data: parkingLots}) // Returns a list of parking lots
     } catch (err) {
         console.error(err)
-        return res.status(400).json({'Error': 'Getting the parking lots'})
+        return res.status(400).json({'Error': 'Getting the parking lots'}) // Couldn't get the parking lots
     }
 }
 
+// Updates a parking lot
 const updateParkingLot = async (req, res) => {
     try {
-        const updatedParkingLot = await ParkingLot
+        const updatedParkingLot = await ParkingLot // Updates an specific parking lot
         .findByIdAndUpdate(
             req.params.id,
             {... req.body},
@@ -55,28 +59,29 @@ const updateParkingLot = async (req, res) => {
         .lean()
         .exec()
 
-        if (!updatedParkingLot) {
+        if (!updatedParkingLot) { // Checks if the parking lot to update existed
             return res.status(400).json({ 'Error': 'Updating the parking lot'})
         }
 
-        return res.status(200).json({ data: updatedParkingLot})
+        return res.status(200).json({ data: updatedParkingLot}) // Returns the updated parking lot data 
     } catch (err) {
         console.error(err)
-        return res.status(400).json({'Error': 'The parking lot id doesn\'t not exist'})
+        return res.status(400).json({'Error': 'The parking lot id doesn\'t not exist'}) // Couldn't update the parking lot
     }
 }
 
+// Deletes the parking lot
 const deleteParkingLot = async (req, res) => {
     try {
-        const deletedParkingLot = await ParkingLot
+        const deletedParkingLot = await ParkingLot // Deletes an specific parking lot
         .findByIdAndRemove(req.params.id)
         .exec()
 
-        if (!deletedParkingLot) {
+        if (!deletedParkingLot) { // Checks if the parking lot existed
             return res.status(400).json({'Error': 'Parking lot id doesn\'t exist'})
         }
 
-        return res.status(200).json({delete: deletedParkingLot})
+        return res.status(200).json({delete: deletedParkingLot}) // Returns the deleted parking lot data
 
     } catch (err) {
         console.error(err)
